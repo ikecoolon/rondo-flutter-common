5ed32b0a1add675967f25d241e051a27c2234a81
ikecoolon <ikecoolon@hotmail.com>
2020-10-12 16:20:55 +0800

    增加新的组件及公共方法

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,11 @@ export 'src/net/PushAndRedirect.dart';
 export 'src/net/ResultData.dart';
 
 export 'src/sm3/Sm3Utils.dart';
+export 'src/other/RondoKey.dart';
 export 'src/other/RondoUtils.dart';
 export 'src/widget/AoStarCommonWidget.dart';
 export 'src/widget/AostarWebViewCommonPage.dart';
+
 
 
 
--- lib/src/other/RondoUtils.dart
+++ lib/src/other/RondoUtils.dart
@@ -1,18 +1,27 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:device_info/device_info.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:rondo_flutter_common/main.dart';
 import 'package:rondo_flutter_common/src/other/RondoKey.dart';
 
-/**
- * author by zyl
- */
+/// author by zyl
 class RondoUtils {
+
+  static final double MILLIS_LIMIT = 1000.0;
+  static final double SECONDS_LIMIT = 60 * MILLIS_LIMIT;
+  static final double MINUTES_LIMIT = 60 * SECONDS_LIMIT;
+  static final double HOURS_LIMIT = 24 * MINUTES_LIMIT;
+  static final double DAYS_LIMIT = 30 * HOURS_LIMIT;
+
   static FlutterWebviewPlugin flutterWebviewPlugin = FlutterWebviewPlugin();
   static StreamSubscription<WebViewStateChanged> _onStateChanged;
+  static DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
 
   /// 通用登录后逻辑
   ///[ context ] 上下文
@@ -59,5 +68,154 @@ class RondoUtils {
 //        return Future.value(result);
       }
     });
+  }
+
+  static Future<Null> showLoadingDialog(BuildContext context,
+      {int dismissTimeout = 60}) {
+    bool timeout = false;
+    Timer _timerDia;
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          //为通用调用设置超时关闭时间
+          return StatefulBuilder(builder: (context, mSetState) {
+            _timerDia = Timer(Duration(seconds: dismissTimeout), () {
+              timeout = true;
+
+//            Navigator.maybePop(context);
+            });
+            // mSetState((){});
+            if (timeout) {
+              _timerDia?.cancel();
+            }
+            return GestureDetector(
+              onTap: () async {
+                if (timeout) {
+                  Navigator.maybePop(context);
+                }
+              },
+              child: new Material(
+                  color: Colors.transparent,
+                  child: WillPopScope(
+                    onWillPop: () async {
+                      return timeout;
+                    },
+                    child: Center(
+                      child: new Container(
+                        width: 200.0,
+                        height: 200.0,
+                        padding: new EdgeInsets.all(4.0),
+                        decoration: new BoxDecoration(
+                          color: Colors.transparent,
+                          //用一个BoxDecoration装饰器提供背景图片
+                          borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                        ),
+                        child: new Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            new Container(
+                                child: SpinKitCubeGrid(color: Colors.white)),
+                            new Container(height: 10.0),
+                            new Container(
+                                child: new Text('加载中...',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                    ))),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )),
+            );
+          });
+        });
+  }
+
+  //至少一个汉字+字母+数值+下划线+中文+中文标点符号+英文标点符号
+  static String getRegHanZi() {
+    return '[a-zA-Z0-9_\u4e00-\u9fa5.,?!。，？！]+';
+  }
+
+  //不允许输入的字符
+  static String getRegNotAllow() {
+    return '[@\$%^&<>`]+';
+  }
+
+  // 不允许输入特殊字符
+  static String checkTszf() {
+    return '[A-Za-z0-9-_\u4e00-\u9fa5]+';
+  }
+
+  //只能输入汉字
+  static String getRegOnlyHanZi() {
+    return '^[\u4e00-\u9fa5]+';
+  }
+
+  static String getRegPhone() {
+    return '^((13[0-9])|(15[^4])|(166)|(17[0-8])|(18[0-9])|(19[0-9])|(147,145))\\d{8}\$';
+  }
+
+  static String getRegMobilePhone() {
+    return '^[0-9]+([-])?[0-9]{0,2}';
+  }
+
+  //非0开头，最多保留两位小数的正数
+  static String getRegNum() {
+    return '(?!00)(?!01)(?!02)(?!03)(?!04)(?!05)(?!06)(?!07)(?!08)(?!09)[0-9]+([.])?[0-9]{0,2}';
+//    return '0.\\d?[1-9]|^[1-9]{1}\\d*.\\d?[1-9]|^[1-9]{1}\\d*';
+//    return '[0-9]+([.])?[0-9]{0,2}';
+  }
+
+  //最多保留两位小数的正数
+  static String getRegPrice() {
+    return '[0-9]+([.])?[0-9]{0,2}';
+  }
+
+  static String getDateStr(DateTime date) {
+    if (date == null || date.toString() == null) {
+      return "";
+    } else if (date.toString().length < 10) {
+      return date.toString();
+    }
+    return date.toString().substring(0, 10);
+  }
+
+  static String getDateTimeStr(DateTime date) {
+    if (date == null || date.toString() == null) {
+      return "";
+    } else if (date.toString().length < 10) {
+      return date.toString();
+    }
+    return date.toString().substring(0, 19);
+  }
+
+  static Future<bool> isIosSimulator() async {
+    if (Platform.isIOS) {
+      IosDeviceInfo iosDeviceInfo = await deviceInfo.iosInfo;
+      return !iosDeviceInfo.isPhysicalDevice;
+    } else {
+      return false;
+    }
+  }
+
+  ///日期格式转换
+  static String getNewsTimeStr(DateTime date) {
+    int subTime =
+        DateTime.now().millisecondsSinceEpoch - date.millisecondsSinceEpoch;
+
+    if (subTime < MILLIS_LIMIT) {
+      return "刚刚";
+    } else if (subTime < SECONDS_LIMIT) {
+      return (subTime / MILLIS_LIMIT).round().toString() + " 秒前";
+    } else if (subTime < MINUTES_LIMIT) {
+      return (subTime / SECONDS_LIMIT).round().toString() + " 分钟前";
+    } else if (subTime < HOURS_LIMIT) {
+      return (subTime / MINUTES_LIMIT).round().toString() + " 小时前";
+    } else if (subTime < DAYS_LIMIT) {
+      return (subTime / HOURS_LIMIT).round().toString() + " 天前";
+    } else {
+      return getDateStr(date);
+    }
   }
 }
